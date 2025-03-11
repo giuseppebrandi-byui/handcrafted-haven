768f5119fd5f1009687f2ad76cf2770297130167
giuseppebrandi-byui <bra22041@byui.edu>
2025-03-11 23:43:13 +0000

    Corrected typo in not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
     <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
       <h1 className="text-3xl font-bold mb-4">Not Found</h1>
       <p className="text-destructive">Sorry, we could not find the requested page</p>
-      <Button variant="outline" className="mt-4 ml-2" onClick={ () => (window.location.href = "/")} >back to Home</Button>
+      <Button variant="outline" className="mt-4 ml-2" onClick={ () => (window.location.href = "/")} >Back to Home</Button>
     </div>
   </div>;
 }
