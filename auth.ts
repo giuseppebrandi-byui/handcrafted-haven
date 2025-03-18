879c0731ab17f61b1fc8ad6a72d3f51028cfef26
liviagalletti <liligalletti@gmail.com>
2025-03-17 23:29:11 -0300

    add. to cart 46 - 53

--- auth.ts
+++ auth.ts
@@ -4,11 +4,13 @@ import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
     signIn: "/sign-in",
-    error: "/sign-in"
+    error: "/sign-in",
   },
   session: {
     strategy: "jwt",
@@ -19,54 +21,57 @@ export const config = {
     CredentialsProvider({
       credentials: {
         email: { type: "email" },
-        password: {type: "password"}
+        password: { type: "password" },
       },
-      async authorize(credentials) { 
+      async authorize(credentials) {
         if (credentials == null) return null;
 
         // Find user in database
         const user = await prisma.user.findFirst({
           where: {
-            email: credentials.email as string
-          }
+            email: credentials.email as string,
+          },
         });
 
         // Check if user exists and if the password matches
-        if (user && user.password) { 
-          const isMatch = compareSync(credentials.password as string, user.password);
+        if (user && user.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password
+          );
 
           // If password is correct, return user
-          if (isMatch) { 
+          if (isMatch) {
             return {
               id: user.id,
               name: user.name,
               email: user.email,
-              role: user.role
-            }
+              role: user.role,
+            };
           }
         }
         // If user does not exist or password does not match return null
         return null;
-      }
-    })
+      },
+    }),
   ],
   callbacks: {
-    async session({ session, user, trigger, token }: any) { 
+    async session({ session, user, trigger, token }: any) {
       // Set user ID from the token
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
 
       // If there is an update, set the user name
-      if (trigger === "update") { 
+      if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
     },
-    async jwt({token, user, trigger, session} : any) {
+    async jwt({ token, user, trigger, session }: any) {
       // Assign user fields to token
-      if (user) { 
+      if (user) {
         token.role = user.role;
 
         // If user has no name then use email
@@ -76,13 +81,36 @@ export const config = {
           // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
-            data: {name: token.name}
-          })
+            data: { name: token.name },
+          });
         }
       }
       return token;
-    }
-  }
+    },
+    authorized({ request, auth }: any) {
+      // check for session cart cookie
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate new session cart id cookie
+        const sessionCartId = crypto.randomUUID();
+        // Clone the req headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create New response and add the new headers
+
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders
+          }
+        });
+        //Set newly generated sessionCartId in the response cookies
+        response.cookies.set("sessionCartId", sessionCartId);
+
+        return response;
+      } else {
+        return true;
+      }
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,36 +1,82 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
+import { Plus, Minus, Loader } from "lucide-react";
+import { CartItem, Cart } from "@/types";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast(res.message);
-      return;
-    }
+      if (!res.success) {
+        toast(res.message);
+        return;
+      }
 
-    //Handle success add to cart 
-    toast.success(`${item.name} added to cart`, {
+      //Handle success add to cart
+      toast.success(res.message, {
         action: {
           label: "Go to Cart",
           onClick: () => {
-            router.push('/cart')
+            router.push("/cart");
           },
         },
         className: "bg-primary text-white hover:bg-gray-800",
       });
+    });
   };
 
-  return (
+  const handleRemoveFromCart = async () => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+
+      if (res.success) {
+        toast.success(res.message);
+      } else {
+        toast.error(res.message);
+      }
+
+      return;
+    });
+  };
+
+  const exisItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return exisItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
+      </Button>
+      <span className="px-2">{exisItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+      {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus/>Add To Cart
+      {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
+      Add To Cart
     </Button>
   );
 };
