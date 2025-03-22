48a688c0d4f4d14a50491fb900c58d07ca4bca31
giuseppebrandi-byui <bra22041@byui.edu>
2025-03-22 23:48:50 +0000

    Finalize cart & shipping features

--- auth.ts
+++ auth.ts
@@ -70,6 +70,7 @@ export const config = {
     async jwt({token, user, trigger, session} : any) {
       // Assign user fields to token
       if (user) { 
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name then use email
