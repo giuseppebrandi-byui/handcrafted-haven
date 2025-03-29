4dc6706723c324f8dc53d7e5e67fc28ef5ee658c
Jess Nelson <jemane.19@gmail.com>
2025-03-28 18:36:50 -0600

    section 9 (has errors)

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -2,7 +2,7 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Handcrafted Haven";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Handcrafted Haven aims to provide a platform for artisans and crafters to showcase and sell their unique handcrafted items.";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
-
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 12;
 export const signInDefaultValues = {
   email: "",
   password: ""
