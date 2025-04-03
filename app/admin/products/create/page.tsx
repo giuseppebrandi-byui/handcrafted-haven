edf2ab1d21ca6ee76f5ae23b5a7c97bad3a68fba
giuseppebrandi-byui <bra22041@byui.edu>
2025-04-03 15:31:14 +0100

    Enable artisan to edit product

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,18 +1,21 @@
 import { Metadata } from "next";
 import ProductForm from "@/components/admin/product-form";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Create Product",
 }
 
-const CreateProductPage = () => {
+const CreateProductPage = async () => {
+  const session = await auth(); // Get the session using your custom auth method
   return (
-    <>
+     <SessionProvider session={session}>
       <h2 className="h2-bold">Create Product</h2>
       <div className="my-8">
         <ProductForm type="Create"/>
       </div>
-    </>
+    </SessionProvider>
   );
 }
  
