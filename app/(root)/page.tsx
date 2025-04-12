0bda7a6538acf6d2c8edacc2f2fa0edc8a481600
Zamarron <emilio.zamarrons@gmail.com>
2025-04-12 00:14:55 -0600

    working in my part

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,15 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import DealCountdown from "@/components/deal-countdown";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && <ProductCarousel data={featuredProducts} />}
       <ProductList data={latestProducts} title="Latest Products" limit={4} />
       <DealCountdown />
     </>
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -140,3 +140,31 @@ export async function getStoriesByUsername(username: string) {
     }
   })
 }
+
+
+// Get all categories 
+export async function GetAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true,
+  });
+  
+  return data;
+}
+
+
+// Get featured Products 
+export async function getFeaturedProducts() {
+    const data = await prisma.product.findMany({
+      where: {isFeatured: true},
+      orderBy: {createdAt: 'desc'},
+      take: 4,
+    });
+
+    return convertToPlainObject(data);
+}
+
+
+
+
+
