61070d802a928f00a27c181fbd3f62eff310f109
Zamarron <emilio.zamarrons@gmail.com>
2025-04-13 20:16:52 -0600

    Adding search bar

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -2,6 +2,7 @@ import ProductCard from "@/components/shared/product/product-card";
 import { Button } from "@/components/ui/button";
 import { getAllProducts, getAllCategories } from "@/lib/actions/product.actions";
 import Link from "next/link";
+import { title } from "process";
 
 const prices = [
   {
@@ -29,6 +30,49 @@ const prices = [
 const ratings = [4, 3, 2, 1];
 
 const sortOrders = ["newest", "lowest", "highest", "rating"];
+
+export async function gerateMetadata(
+  props:{
+    searchParams: Promise<{
+      q:string;
+      category:string;
+      price:string;
+      rating:string;
+    }>
+  }
+){
+  const{ 
+    q='all',
+    category='all',
+    price='all',
+    rating='all',
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== 'all' && q.trim() !== '';
+  const isCategorySet = category && category !== 'all' && category.trim() !== '';
+  const isPriceSet = price && price !== 'all' && price.trim() !== '';
+  const isRatingSet = rating && rating !== 'all' && rating.trim() !== '';
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet ) {
+    return{
+      title: `
+        Search ${isQuerySet ? q : ''}
+        ${isCategorySet ? `: Category ${category}`: ''}
+        ${isPriceSet? `: Price ${price}`: '' }
+        ${isRatingSet ? `: Rating ${rating}` : ''}`
+    };
+  } else{
+    return{
+      title: 'Search Products'
+    };
+  }
+
+  return{
+    title:'Search',
+
+  };
+}
+
 
 const SearchPage = async (props: {
   searchParams: Promise<{
